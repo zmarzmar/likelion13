302a1fb0a096423a850f88c5ee4ca4f6b3fb3f59
Dongmyoung Min <zmarzmzm@naver.com>
2025-03-06 15:20:42 +0900

    first commit

---
+++ src/test/java/com/example/likelion/LikelionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
